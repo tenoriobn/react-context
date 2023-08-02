a9a3088b945797f5fe538ab99d3dc26d4d5cf972
Bruno Tenório <tenoriobn@gmail.com>
2023-08-02 09:40:45 -0300

    Usando o Context nas telas

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -9,60 +9,57 @@ import {
   InputLabel,
   InputAdornment 
 } from '@material-ui/core';
+
 import { useHistory } from 'react-router-dom';
 import { UsuarioContext } from 'common/context/Usuario';
+import { useContext } from 'react';
 
 function Login() {
   const history = useHistory();
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
 
   return (
     <Container>
-      <UsuarioContext.Consumer>
-        {( {nome, setNome, saldo, setSaldo} ) => (
+      <Titulo>
+        Insira o seu nome
+      </Titulo>
+      
+      <InputContainer>
+        <InputLabel>
+          Nome
+        </InputLabel>
 
-          <>
-            <Titulo>
-              Insira o seu nome
-            </Titulo>
-            <InputContainer>
-              <InputLabel>
-                Nome
-              </InputLabel>
-              <Input
-                value={nome}
-                onChange={(event) => setNome(event.target.value)}
-                type="text"
-              />
-            </InputContainer>
-            
-            <InputContainer>
-              <InputLabel>
-                Saldo
-              </InputLabel>
-              
-              <Input
-                value={saldo}
-                onChange={(event) => setSaldo(event.target.value)}
-                type="number"
-                startAdornment={
-                  <InputAdornment position="start">
-                    R$
-                  </InputAdornment>
-                }
-              />
-            </InputContainer>
-            
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => history.push('/feira')}
-            >
-              Avançar
-            </Button>
-          </>
-
-        )}
-      </UsuarioContext.Consumer>
+        <Input
+          value={nome}
+          onChange={(event) => setNome(event.target.value)}
+          type="text"
+        />
+      </InputContainer>
+      
+      <InputContainer>
+        <InputLabel>
+          Saldo
+        </InputLabel>
+        
+        <Input
+          value={saldo}
+          onChange={(event) => setSaldo(event.target.value)}
+          type="number"
+          startAdornment={
+            <InputAdornment position="start">
+              R$
+            </InputAdornment>
+          }
+        />
+      </InputContainer>
+      
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => history.push('/feira')}
+      >
+        Avançar
+      </Button>
     </Container>
   )
 };
