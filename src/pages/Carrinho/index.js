41e2f30d9e42d459d29ec13b86c16b6d8e51f116
Bruno Tenório <tenoriobn@gmail.com>
2023-08-08 19:49:54 -0300

    Mudando a forma de pagamento

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,4 +1,4 @@
-import { Button, Snackbar, InputLabel } from '@material-ui/core';
+import { Button, Snackbar, InputLabel, Select, MenuItem } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
 import { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
@@ -10,13 +10,12 @@ import { PagamentoContext } from 'common/context/Pagamento';
 function Carrinho() {
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const { carrinho } = useCarrinhoContext();
-  const { formaPagamento } = useContext(PagamentoContext)
+  const { tiposPagamentos, formaPagamento, setFormaPagamento } = useContext(PagamentoContext)
   const history = useHistory();
 
   return (
     <Container>
       <Voltar onClick={() => history.goBack()} />
-      {formaPagamento.nome}
       <h2>
         Carrinho
       </h2>
@@ -28,6 +27,16 @@ function Carrinho() {
       ))}
       <PagamentoContainer>
         <InputLabel> Forma de Pagamento </InputLabel>
+        <Select
+          value={formaPagamento.id}
+          onChange={(event) => setFormaPagamento(event.target.value)}
+        >
+          {tiposPagamentos.map(pagamento => (
+            <MenuItem value={pagamento.id} key={pagamento.id}>
+              {pagamento.nome}
+            </MenuItem>
+          ))}
+        </Select>
       </PagamentoContainer>
       <TotalContainer>
           <div>
